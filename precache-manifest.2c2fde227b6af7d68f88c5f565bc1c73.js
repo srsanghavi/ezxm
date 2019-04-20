a6297738042316b2455b15b3d59d79dc7b87dfec
shashwat <shashwat@mobile-sphere.com>
2019-04-20 18:43:34 -0400

    Updates

---
+++ precache-manifest.2c2fde227b6af7d68f88c5f565bc1c73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "17a8af80f303e573faa5",
+    "url": "/ezxm/static/js/runtime~main.dd36c09e.js"
+  },
+  {
+    "revision": "392879360cc98aec8bef",
+    "url": "/ezxm/static/js/main.95ea8706.chunk.js"
+  },
+  {
+    "revision": "ff99d5375c03250172fc",
+    "url": "/ezxm/static/js/2.b7bf2c31.chunk.js"
+  },
+  {
+    "revision": "392879360cc98aec8bef",
+    "url": "/ezxm/static/css/main.d0157f52.chunk.css"
+  },
+  {
+    "revision": "018312c24aa292eaf6bf2a6cda5d1e87",
+    "url": "/ezxm/index.html"
+  }
+];
